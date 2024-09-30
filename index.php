a30c9e3d0c5e1bcb217778377d2de61bdc28efcc
Sainan <sainan@calamity.inc>
2024-09-30 07:55:21 +0200

    Assign categories to tools & add navbar

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
 $tools = json_decode(file_get_contents("tools.json"), true);
+
+function getToolBySlug($slug)
+{
+	global $tools;
+
+	foreach ($tools as $tool)
+	{
+		if ($tool["slug"] == $slug)
+		{
+			return $tool;
+		}
+	}
+	return null;
+}
+
+function getToolsByCategory()
+{
+	global $tools;
+
+	$tools_by_category = [];
+	foreach ($tools as $tool)
+	{
+		$tools_by_category[$tool["category"]][] = $tool;
+	}
+	return $tools_by_category;
+}
+
 if ($_SERVER["REQUEST_URI"] == "/")
 {
 	?>
@@ -16,8 +43,15 @@ if ($_SERVER["REQUEST_URI"] == "/")
 			<h1 class="mt-3">omnitool.app</h1>
 			<p>The following tools are currently available:</p>
 			<ul>
-				<?php foreach ($tools as $tool): ?>
-					<li><a href="<?=$tool["slug"];?>"><?=$tool["name"];?></a></li>
+				<?php foreach (getToolsByCategory() as $category => $tools_in_category): ?>
+					<li>
+						<?=$category;?>
+						<ul>
+							<?php foreach ($tools_in_category as $tool): ?>
+								<li><a href="<?=$tool["slug"];?>"><?=$tool["name"];?></a></li>
+							<?php endforeach; ?>
+						</ul>
+					</li>
 				<?php endforeach; ?>
 			</ul>
 		</div>
@@ -27,20 +61,6 @@ if ($_SERVER["REQUEST_URI"] == "/")
 }
 else
 {
-	function getToolBySlug($slug)
-	{
-		global $tools;
-
-		foreach ($tools as $tool)
-		{
-			if ($tool["slug"] == $slug)
-			{
-				return $tool;
-			}
-		}
-		return null;
-	}
-
 	$slug = substr($_SERVER["REQUEST_URI"], 1);
 	$slug = strstr($slug, "#", true) ?: $slug;
 	$tool = getToolBySlug(strtolower($slug));
@@ -56,6 +76,7 @@ else
 		header("Location: /".$tool["slug"]);
 		exit;
 	}
+
 	http_response_code(200);
 	?>
 	<!DOCTYPE html>
@@ -67,6 +88,28 @@ else
 	</head>
 	<body>
 		<script src="theme.js"></script>
+		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+			<div class="container">
+				<a class="navbar-brand" href="/">omnitool.app</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<?php foreach (getToolsByCategory() as $category => $tools_in_category): ?>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle<?php if ($category == $tool["category"]) { echo " active"; } ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><?=$category;?></a>
+								<ul class="dropdown-menu">
+									<?php foreach ($tools_in_category as $nav_tool): ?>
+										<li><a class="dropdown-item<?php if ($nav_tool["slug"] == $tool["slug"]) { echo " active"; } ?>" href="/<?=$nav_tool["slug"];?>"><?=$nav_tool["name"];?></a></li>
+									<?php endforeach; ?>
+								</ul>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			</div>
+		</nav>
 		<div class="container">
 			<h1 class="mt-3"><?=$tool["name"];?></h1>
 			<?php
@@ -81,7 +124,7 @@ else
 			<script type="pluto">tool = {}</script>
 
 			<label for="input" class="form-label">Input</label>
-			<textarea id="input" class="form-control" style="height:calc(50vh - 160px)" autofocus></textarea>
+			<textarea id="input" class="form-control" style="height:calc(50vh - 180px)" autofocus></textarea>
 
 			<?php if (array_key_exists("numeric", $tool)): ?>
 				<label for="format" class="form-label mt-3">Output Format</label>
@@ -95,7 +138,7 @@ else
 			<?php endif; ?>
 
 			<label for="output" class="form-label mt-3">Output</label>
-			<textarea id="output" class="form-control" style="height:calc(50vh - 160px)" readonly></textarea>
+			<textarea id="output" class="form-control" style="height:calc(50vh - 180px)" readonly></textarea>
 
 			<?php if (array_key_exists("related", $tool)): ?>
 				<h2 class="mt-3">Related Tools</h2>
@@ -111,8 +154,8 @@ else
 		<script src="platform.js"></script>
 		<script type="pluto" src="platform.pluto"></script>
 		<script type="pluto" src="tools/<?=$tool["slug"];?>.pluto"></script>
+	    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 	</body>
 	</html>
 	<?php
 }
-?>
