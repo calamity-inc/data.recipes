c62ec2c78020753d5cc2ee249de952c096b4d87d
Sainan <sainan@calamity.inc>
2024-09-30 07:39:16 +0200

    Fix some markup blunders

--- index.php
+++ index.php
@@ -10,17 +10,19 @@ if ($_SERVER["REQUEST_URI"] == "/")
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	</head>
-	<div class="container">
+	<body>
 		<script src="theme.js"></script>
-		<h1 class="mt-3">omnitool.app</h1>
-		<p>The following tools are currently available:</p>
-		<ul>
-			<?php foreach ($tools as $tool): ?>
-				<li><a href="<?=$tool["slug"];?>"><?=$tool["name"];?></li>
-			<?php endforeach; ?>
+		<div class="container">
+			<h1 class="mt-3">omnitool.app</h1>
+			<p>The following tools are currently available:</p>
+			<ul>
+				<?php foreach ($tools as $tool): ?>
+					<li><a href="<?=$tool["slug"];?>"><?=$tool["name"];?></a></li>
+				<?php endforeach; ?>
 			</ul>
 		</div>
-		</html>
+	</body>
+	</html>
 	<?php
 }
 else
@@ -63,53 +65,53 @@ else
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	</head>
-	<div class="container">
+	<body>
 		<script src="theme.js"></script>
-
-		<h1 class="mt-3"><?=$tool["name"];?></h1>
-		<?php
-		if (array_key_exists("description", $tool))
-		{
-			?>
-			<p><?=$tool["description"];?></p>
+		<div class="container">
+			<h1 class="mt-3"><?=$tool["name"];?></h1>
 			<?php
-		}
-		?>
+			if (array_key_exists("description", $tool))
+			{
+				?>
+				<p><?=$tool["description"];?></p>
+				<?php
+			}
+			?>
 
-		<script type="pluto">tool = {}</script>
+			<script type="pluto">tool = {}</script>
 
-		<label for="input" class="form-label">Input</label>
-		<textarea id="input" class="form-control" style="height:calc(50vh - 160px)" autofocus></textarea>
+			<label for="input" class="form-label">Input</label>
+			<textarea id="input" class="form-control" style="height:calc(50vh - 160px)" autofocus></textarea>
 
-		<?php if (array_key_exists("numeric", $tool)): ?>
-			<label for="format" class="form-label mt-3">Output Format</label>
-			<select id="format" class="form-control">
-				<option>Decimal (Signed)</option>
-				<option>Decimal (Unsigned)</option>
-				<option>Hexadecimal (Unsigned)</option>
-			</select>
+			<?php if (array_key_exists("numeric", $tool)): ?>
+				<label for="format" class="form-label mt-3">Output Format</label>
+				<select id="format" class="form-control">
+					<option>Decimal (Signed)</option>
+					<option>Decimal (Unsigned)</option>
+					<option>Hexadecimal (Unsigned)</option>
+				</select>
 
-			<script type="pluto">tool.numeric = true</script>
-		<?php endif; ?>
+				<script type="pluto">tool.numeric = true</script>
+			<?php endif; ?>
 
-		<label for="output" class="form-label mt-3">Output</label>
-		<textarea id="output" class="form-control" style="height:calc(50vh - 160px)" readonly></textarea>
+			<label for="output" class="form-label mt-3">Output</label>
+			<textarea id="output" class="form-control" style="height:calc(50vh - 160px)" readonly></textarea>
 
-		<?php if (array_key_exists("related", $tool)): ?>
-			<h2 class="mt-3">Related Tools</h2>
-			<ul>
-				<?php foreach ($tool["related"] as $rslug): ?>
-					<li><a href="/<?=$rslug;?>"><?=getToolBySlug($rslug)["name"];?></a></li>
-				<?php endforeach; ?>
-			</ul>
-		<?php endif; ?>
-
+			<?php if (array_key_exists("related", $tool)): ?>
+				<h2 class="mt-3">Related Tools</h2>
+				<ul>
+					<?php foreach ($tool["related"] as $rslug): ?>
+						<li><a href="/<?=$rslug;?>"><?=getToolBySlug($rslug)["name"];?></a></li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
+		</div>
 		<script src="https://pluto-lang.org/wasm-builds/out/libpluto/0.9.5/libpluto.js"></script>
 		<script src="https://pluto-lang.org/PlutoScript/plutoscript.js"></script>
 		<script src="platform.js"></script>
 		<script type="pluto" src="platform.pluto"></script>
 		<script type="pluto" src="tools/<?=$tool["slug"];?>.pluto"></script>
-	</div>
+	</body>
 	</html>
 	<?php
 }
