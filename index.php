da70c3f034637a5a96fc9e7deaa51db40465e6a2
Sainan <sainan@calamity.inc>
2024-10-03 22:12:37 +0200

    Fix inconsistent indentation

--- index.php
+++ index.php
@@ -154,7 +154,7 @@ else
 		<script src="platform.js"></script>
 		<script type="pluto" src="platform.pluto"></script>
 		<script type="pluto" src="tools/<?=$tool["slug"];?>.pluto"></script>
-	    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 	</body>
 	</html>
 	<?php
